852fb1ff5ba67679548d32124d7f01a4ee5c1f61
windpasc <christoph.windpassinger@msg.group>
2020-09-23 16:17:16 +0200

    Added forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const request = require('request');
 
 const forecast = (longitude, latitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=f7e74700ef9c5b30b99db563a26ccf60&query=${encodeURIComponent(
@@ -7,19 +7,24 @@ const forecast = (longitude, latitude, callback) => {
 
   request({ url, json: true }, (error, response) => {
     if (error) {
-      const lowLevelError = "Unable to connect to weather service!";
+      const lowLevelError = 'Unable to connect to weather service!';
       callback(lowLevelError, undefined);
     } else if (response.body.error) {
       // console.log(response.body.error);
 
-      const apiError = "Unable to find location!";
+      const apiError = 'Unable to find location!';
       callback(apiError, undefined);
     } else {
       const description = response.body.current.weather_descriptions[0];
       const temp = response.body.current.temperature;
       const feelsLike = response.body.current.feelslike;
+      const humidity = response.body.current.humidity;
 
-      const result = `${description}. It is currently ${temp} degrees out. It feels like ${feelsLike} degrees.`;
+      const result = `${description}. 
+      It is currently ${temp} degrees out. 
+      It feels like ${feelsLike} degrees.
+      The humidity is ${humidity}%.`;
+
       callback(undefined, result);
     }
   });
